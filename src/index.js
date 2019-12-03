fe65bd10d1bee093adc89a91d6a79f8e40c70c2b
Ksenia <test1@test.ru>
2019-12-03 19:50:05 +0300

    задача 1 переписана через стрелочную функцию

--- src/index.js
+++ src/index.js
@@ -9,9 +9,7 @@
    delayPromise(3) // вернет promise, который будет разрешен через 3 секунды
  */
 function delayPromise(seconds) {
-    return new Promise(function(resolve) {
-        setTimeout(resolve, seconds * 1000);
-    });
+    return new Promise(resolve => setTimeout(resolve, seconds * 1000));
 }
 
 /*
