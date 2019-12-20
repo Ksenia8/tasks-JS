af87c01fde968fd3bcafa0edee439fc74fe2e97f
Ksenia <test1@test.ru>
2019-12-20 22:39:39 +0300

    добавлена карта, обратное геокодирование, класстеризация

--- src/index.js
+++ src/index.js
@@ -1,69 +1,107 @@
 ymaps.ready(init);
 
+// метки
+let placemarks = [
+        {   
+            coords: [59.97, 30.31],
+            hintContent: '<div class="map__hint">ул. Литераторов, д. 19</div>'
+            
+        },
+        {
+            coords: [59.94, 30.25],
+            hintContent: '<div class="map__hint">Малый проспект В О, д 64</div>'
+            
+        },
+        {
+            coords: [59.93, 30.34],
+            hintContent: '<div class="map__hint">наб. реки Фонтанки, д. 56</div>'
+            
+        },
+        {
+            coords: [59.92, 30.31],
+            hintContent: '<div class="map__hint">наб. реки Фонтанки, д. 123</div>'
+            
+        }
+    ],
+    geoObjects= [];
+
 function init() {
-    var myPlacemark,
-        myMap = new ymaps.Map('map', {
-            center: [55.753994, 37.622093],
-            zoom: 9
-        }, {
-            searchControlProvider: 'yandex#search'
-        });
-
-    // Слушаем клик на карте.
-    myMap.events.add('click', function (e) {
-        var coords = e.get('coords');
-
-        // Если метка уже создана – просто передвигаем ее.
-        if (myPlacemark) {
-            myPlacemark.geometry.setCoordinates(coords);
-        }
-        // Если нет – создаем.
-        else {
-            myPlacemark = createPlacemark(coords);
-            myMap.geoObjects.add(myPlacemark);
-            // Слушаем событие окончания перетаскивания на метке.
-            myPlacemark.events.add('dragend', function () {
-                getAddress(myPlacemark.geometry.getCoordinates());
-            });
-        }
-        getAddress(coords);
+    let map = new ymaps.Map('map', {
+        center: [59.94, 30.32],
+        zoom: 12,
+        controls: ['zoomControl'],
+        behaviors: ['drag']
+    }, {
+        searchControlProvider: 'yandex#search'
     });
 
-    // Создание метки.
-    function createPlacemark(coords) {
-        return new ymaps.Placemark(coords, {
-            iconCaption: 'поиск...'
-        }, {
-            preset: 'islands#violetDotIconWithCaption',
-            draggable: true
-        });
-    }
+    // Обработчик события клик по карте
+    map.events.add('click', function (e) {
+        let coords = e.get('coords');
+        
+        getAddress(coords);
+        // для каждого объекта создаем метки (хинт и балун)
+        for (let i = 0; i < placemarks.length; i++) {
+            geoObjects[i] = new ymaps.Placemark([placemarks[i].coords[0], placemarks[i].coords[1]],
+                {
+                    hintContent: placemarks[i].hintContent,
+                    // balloonContent: placemarks[i].balloonContent.join(''),
+                    balloonContent: '<div class="review__header">' +
+                    '<div class="review__header-point">'+
+                    '<a href="#" class="review__point-link"><img src="./img/point.png" alt="Close" class="review__point-img"></a>'+
+                    '</div>'+
+                    '<div class="review__header-address"><i></i></div>'+
+                    '</div>'+'<div class="review__list">' +
+                        '<div class="review__list-info">' +
+                        '<div class="review__list-author"> {{author}}</div>'+
+                        '<div class="review__list-place"> {{place}} </div>'+
+                        '<div class="review__list-date"> {{date}} </div>'+
+                        '</div>'+
+                        '<div class="review__list-text"> {{text}} </div>'+
+                        '<div class="noReview">Отзывов пока нет</div>'+'<form action="" class="review__form">'+
+                    '<h2 class = "review__form-header">Ваш отзыв</h2>'+
+                    '<input type="text" class="review__name" placeholder="Ваше имя">'+
+                    '<input type="text" class="review__place" placeholder="Укажите место">'+
+                    '<textarea name="text" id="review__text" cols="30" rows="10" class="review__text" placeholder="Поделитесь впечатлениями"></textarea>'+
+                    '<button class="btn btn-addReview">Добавить</button></form>'
+                },
+                {
+                    iconLayout: 'default#image',
+                    iconImageHref: 'img/placemark.png',
+                    iconImageSize: [44, 66],
+                    iconImageOffset: [-23, -57]
+                    
+                });
+        }
+        
+    })
 
     // Определяем адрес по координатам (обратное геокодирование).
     function getAddress(coords) {
-        myPlacemark.properties.set('iconCaption', 'поиск...');
+        
         ymaps.geocode(coords).then(function (res) {
-            var firstGeoObject = res.geoObjects.get(0);
+            let firstGeoObject = res.geoObjects.get(0);
+            // строка с адресом
+            let address = firstGeoObject.getAddressLine();
 
-            myPlacemark.properties
-                .set({
-                    // Формируем строку с данными об объекте.
-                    iconCaption: [
-                        // Название населенного пункта или вышестоящее административно-территориальное образование.
-                        firstGeoObject.getLocalities().length ? firstGeoObject.getLocalities() : firstGeoObject.getAdministrativeAreas(),
-                        // Получаем путь до топонима, если метод вернул null, запрашиваем наименование здания.
-                        firstGeoObject.getThoroughfare() || firstGeoObject.getPremise()
-                    ].filter(Boolean).join(', '),
-                    // В качестве контента балуна задаем строку с адресом объекта.
-                    balloonContent: firstGeoObject.getAddressLine()
-                });
+            let obj = {
+                coords: coords,
+                hintContent: '<div class="map__hint">' + address +'</div>'
+            };
+
+            // добавим объект в массив объектов 
+            placemarks.push(obj);
+                        
         });
     }
+
+    let clusterer = new ymaps.Clusterer({
+        
+    });
+
+    map.geoObjects.add(clusterer);
+    clusterer.add(geoObjects);
 }
-
-
-
-
 
 // // 0. инициализировать карту
 // // 1. При клике на карте нужно 
@@ -171,91 +209,3 @@ function init() {
 
 // // }
 
-// ///////////////////////////////////////////////////////
-
-// // 
-
-// ymaps.ready(init);
-
-// function init() {
-//     var placemark,
-//         myMap = new ymaps.Map('map', {
-//             center: [55.753994, 37.622093],
-//             zoom: 9
-//         }, {
-//             searchControlProvider: 'yandex#search'
-//         });
-
-//     // Слушаем клик на карте.
-//     myMap.events.add('click', function (e) {
-//         // var coords = e.get('coords');
-
-// //         // Если метка уже создана – просто передвигаем ее.
-// //         if (myPlacemark) {
-// //             myPlacemark.geometry.setCoordinates(coords);
-// //         }
-// //         // Если нет – создаем.
-// //         else {
-// //             myPlacemark = createPlacemark(coords);
-// //             myMap.geoObjects.add(myPlacemark);
-// //             // Слушаем событие окончания перетаскивания на метке.
-// //             myPlacemark.events.add('dragend', function () {
-// //                 getAddress(myPlacemark.geometry.getCoordinates());
-// //             });
-// //         }
-// //         getAddress(coords);
-// //     });
-
-// //     // Создание метки.
-// //     function createPlacemark(coords) {
-// //         return new ymaps.Placemark(coords, {
-// //             iconCaption: 'поиск...'
-// //         }, {
-// //             preset: 'islands#violetDotIconWithCaption',
-// //             draggable: true
-// //         });
-// //     }
-
-// //     // Определяем адрес по координатам (обратное геокодирование).
-// //     function getAddress(coords) {
-// //         myPlacemark.properties.set('iconCaption', 'поиск...');
-// //         ymaps.geocode(coords).then(function (res) {
-// //             var firstGeoObject = res.geoObjects.get(0);
-
-// //             myPlacemark.properties
-// //                 .set({
-// //                     // Формируем строку с данными об объекте.
-// //                     iconCaption: [
-// //                         // Название населенного пункта или вышестоящее административно-территориальное образование.
-// //                         firstGeoObject.getLocalities().length ? firstGeoObject.getLocalities() : firstGeoObject.getAdministrativeAreas(),
-// //                         // Получаем путь до топонима, если метод вернул null, запрашиваем наименование здания.
-// //                         firstGeoObject.getThoroughfare() || firstGeoObject.getPremise()
-// //                     ].filter(Boolean).join(', '),
-// //                     // В качестве контента балуна задаем строку с адресом объекта.
-// //                     balloonContent: firstGeoObject.getAddressLine()
-// //                 });
-// //         });
-// //     }
-// // }
-
-
-
-
-// ymaps.geocode(placemark.geometry.getCoordinates())
-
-//     .then(function (res) {
-
-//          var geoObject = res.geoObjeсts.get(0);
-
- 
-
-//           placemark.properties.set('balloonContentBody',
-
-//               placemark.properties.get('balloonContentBody') + 
-
-//                 geoObject.properties.get('text'));
-
-//     });
-
-// });
-// }
